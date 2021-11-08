18d2a4d2bd5b2ca3714bcfc42748f2f06b753b6a
Cezar Rosa <cecez86@gmail.com>
2021-11-08 14:32:01 -0300

    dados e propriedade computada para primeiras pontuações e total

--- 99-b-projeto-general/main.js
+++ 99-b-projeto-general/main.js
@@ -1,6 +1,26 @@
 conjuntoDeJogadores = [];
 
 Vue.component('jogador', {
+    computed: {
+        total() {
+            return parseInt(this.um) +
+                parseInt(this.dois) +
+                parseInt(this.tres) +
+                parseInt(this.quatro) +
+                parseInt(this.cinco) +
+                parseInt(this.seis);
+        }
+    },
+    data() {
+        return {
+            um: 0,
+            dois: 0,
+            tres: 0,
+            quatro: 0,
+            cinco: 0,
+            seis: 0
+        }
+    },
     template: `
         <div>
             <div class="table w-full">
@@ -11,7 +31,28 @@ Vue.component('jogador', {
                         </div>
                     </div>
                     <div class="table-row">
-                        <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">Um</div>
+                        <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
+                            <div class="flex">
+                                <div class="w-1/2">Um</div>
+                                <div><input class="w-1/2" type="number" v-model="um"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="table-row">
+                        <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
+                            <div class="flex">
+                                <div class="w-1/2">Dois</div>
+                                <div><input class="w-1/2" type="number" v-model="dois"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="table-row">
+                        <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
+                            <div class="flex">
+                                <div class="w-1/2">Total</div>
+                                <div class="w-1/2">{{ this.total }}</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>

a433736b8985a10fdc8396d5db01fd2a887e4373
Cezar Rosa <cecez86@gmail.com>
2021-11-09 14:12:10 -0300

    versão alfa do jogo general, funcional ao mínimo

--- 99-b-projeto-general/main.js
+++ 99-b-projeto-general/main.js
@@ -8,7 +8,12 @@ Vue.component('jogador', {
                 parseInt(this.tres) +
                 parseInt(this.quatro) +
                 parseInt(this.cinco) +
-                parseInt(this.seis);
+                parseInt(this.seis) +
+                parseInt(this.trinca) +
+                parseInt(this.quadra) +
+                parseInt(this.full_house) +
+                parseInt(this.sequencia) +
+                parseInt(this.general);
         }
     },
     data() {
@@ -18,7 +23,12 @@ Vue.component('jogador', {
             tres: 0,
             quatro: 0,
             cinco: 0,
-            seis: 0
+            seis: 0,
+            trinca: 0,
+            quadra: 0,
+            full_house: 0,
+            sequencia: 0,
+            general: 0
         }
     },
     template: `
@@ -34,18 +44,101 @@ Vue.component('jogador', {
                         <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
                             <div class="flex">
                                 <div class="w-1/2">Um</div>
-                                <div><input class="w-1/2" type="number" v-model="um"></div>
+                                <div><input class="min-w-full" type="number" v-model="um" min="0" max="6" step="1"></div>
                             </div>
                         </div>
                     </div>
+                  
                     <div class="table-row">
                         <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
                             <div class="flex">
                                 <div class="w-1/2">Dois</div>
-                                <div><input class="w-1/2" type="number" v-model="dois"></div>
+                                <div><input class="min-w-full" type="number" v-model="dois" min="0" max="10" step="2"></div>
                             </div>
                         </div>
                     </div>
+
+                  <div class="table-row">
+                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
+                      <div class="flex">
+                        <div class="w-1/2">Três</div>
+                        <div><input type="number" v-model="tres" class="min-w-full" min="0" max="15" step="3"></div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="table-row">
+                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
+                      <div class="flex">
+                        <div class="w-1/2">Quatro</div>
+                        <div><input type="number" v-model="quatro" class="min-w-full" min="0" max="20" step="4"></div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="table-row">
+                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
+                      <div class="flex">
+                        <div class="w-1/2">Cinco</div>
+                        <div><input type="number" v-model="cinco" class="min-w-full" min="0" max="25" step="5"></div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="table-row">
+                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
+                      <div class="flex">
+                        <div class="w-1/2">Seis</div>
+                        <div><input type="number" v-model="seis" class="min-w-full" min="0" max="30" step="6"></div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="table-row">
+                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
+                      <div class="flex">
+                        <div class="w-1/2">Trinca</div>
+                        <div><input type="number" v-model="trinca" class="min-w-full" min="0" max="20" step="20"></div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="table-row">
+                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
+                      <div class="flex">
+                        <div class="w-1/2">Quadra</div>
+                        <div><input type="number" v-model="quadra" class="min-w-full" min="0" max="30" step="30"></div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="table-row">
+                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
+                      <div class="flex">
+                        <div class="w-1/2">Full House</div>
+                        <div><input type="number" v-model="full_house" class="min-w-full" min="0" max="25" step="25"></div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="table-row">
+                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
+                      <div class="flex">
+                        <div class="w-1/2">Sequência</div>
+                        <div><input type="number" v-model="sequencia" class="min-w-full" min="0" max="40" step="40"></div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="table-row">
+                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
+                      <div class="flex">
+                        <div class="w-1/2">General</div>
+                        <div><input type="number" v-model="general" class="min-w-full" min="0" max="50" step="50"></div>
+                      </div>
+                    </div>
+                  </div>
+                  
                     <div class="table-row">
                         <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
                             <div class="flex">
@@ -83,9 +176,9 @@ new Vue({
     methods: {
         novoJogador() {
             const nome = prompt("Qual o nome do jogador?")
-            const novoJogador = { nome: nome }
-
-            conjuntoDeJogadores.push(novoJogador)
+            if (nome !== "") {
+                conjuntoDeJogadores.push({nome: nome})
+            }
         }
     }
 })

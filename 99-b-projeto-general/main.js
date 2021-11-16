b7876298481be51d0b8a8c5514ec4649032e254f
Cezar Rosa <cecez86@gmail.com>
2021-11-16 15:02:11 -0300

    wip: marcando jogada terminada no componente para jogada

--- 99-b-projeto-general/main.js
+++ 99-b-projeto-general/main.js
@@ -21,11 +21,16 @@ Vue.component('jogada', {
       <div class="table-row">
           <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
                 <div class="flex">
-                  <div class="w-1/2">{{ titulo }}</div>
-                  <div><input
+                  <div class="w-1/3">
+                    <input type="checkbox">
+                  </div>
+                  <div class="w-1/3">{{ titulo }}</div>
+                  <div class="w-1/3">
+                    <input
                       @input="$emit('update:pontos', $event.target.value)"
                       :value="pontos"
-                      class="min-w-full" type="number" min="0" :max="max" :step="step"></div>
+                      class="min-w-full" type="number" min="0" :max="max" :step="step">
+                  </div>
                 </div>
           </div>
       </div>
@@ -35,31 +40,31 @@ Vue.component('jogada', {
 Vue.component('jogador', {
     computed: {
         total() {
-            return this.jogadas.reduce((ac, atual) => parseInt(ac.pontos) + parseInt(atual.pontos));
+            return this.jogadas.reduce((ac, atual) => parseInt(ac) + parseInt(atual.pontos), 0);
         }
     },
     data() {
         return {
             jogadas: [
-                { titulo: 'Um', pontos: 0, max: 6, step: 1 },
+                { titulo: 'Um', pontos: 0, max: 5, step: 1 },
                 { titulo: 'Dois', pontos: 0, max: 10, step: 2 },
+                { titulo: 'Três', pontos: 0, max: 15, step: 3 },
+                { titulo: 'Quatro', pontos: 0, max: 20, step: 4 },
+                { titulo: 'Cinco', pontos: 0, max: 25, step: 5 },
+                { titulo: 'Seis', pontos: 0, max: 30, step: 6 },
+                { titulo: 'Trinca', pontos: 0, max: 20, step: 20 },
+                { titulo: 'Quadra', pontos: 0, max: 30, step: 30 },
+                { titulo: 'Full house', pontos: 0, max: 25, step: 25 },
+                { titulo: 'Sequência', pontos: 0, max: 40, step: 40 },
+                { titulo: 'General', pontos: 0, max: 50, step: 50 },
             ],
-            um: 0,
-            dois: 0,
-            tres: 0,
-            quatro: 0,
-            cinco: 0,
-            seis: 0,
-            trinca: 0,
-            quadra: 0,
-            full_house: 0,
-            sequencia: 0,
-            general: 0
         }
     },
     methods: {
         removeJogador(indice) {
-            EventDispatcher.fire('removerJogador', indice);
+            if (confirm('Tem certeza que deseja remover?')) {
+                EventDispatcher.fire('removerJogador', indice);
+            }
         }
     },
     props: ['nome', 'indice'],
@@ -80,93 +85,11 @@ Vue.component('jogador', {
                         v-for="(jogada, index) in this.jogadas"
                         :key="index"
                         :titulo="jogada.titulo"
-                        min="0"
                         :max="jogada.max"
                         :step="jogada.step"
                         :pontos.sync="jogada.pontos"
                     >
                     </jogada>
-
-                  <div class="table-row">
-                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
-                      <div class="flex">
-                        <div class="w-1/2">Três</div>
-                        <div><input type="number" v-model="tres" class="min-w-full" min="0" max="15" step="3"></div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="table-row">
-                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
-                      <div class="flex">
-                        <div class="w-1/2">Quatro</div>
-                        <div><input type="number" v-model="quatro" class="min-w-full" min="0" max="20" step="4"></div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="table-row">
-                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
-                      <div class="flex">
-                        <div class="w-1/2">Cinco</div>
-                        <div><input type="number" v-model="cinco" class="min-w-full" min="0" max="25" step="5"></div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="table-row">
-                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
-                      <div class="flex">
-                        <div class="w-1/2">Seis</div>
-                        <div><input type="number" v-model="seis" class="min-w-full" min="0" max="30" step="6"></div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="table-row">
-                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
-                      <div class="flex">
-                        <div class="w-1/2">Trinca</div>
-                        <div><input type="number" v-model="trinca" class="min-w-full" min="0" max="20" step="20"></div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="table-row">
-                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
-                      <div class="flex">
-                        <div class="w-1/2">Quadra</div>
-                        <div><input type="number" v-model="quadra" class="min-w-full" min="0" max="30" step="30"></div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="table-row">
-                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
-                      <div class="flex">
-                        <div class="w-1/2">Full House</div>
-                        <div><input type="number" v-model="full_house" class="min-w-full" min="0" max="25" step="25"></div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="table-row">
-                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
-                      <div class="flex">
-                        <div class="w-1/2">Sequência</div>
-                        <div><input type="number" v-model="sequencia" class="min-w-full" min="0" max="40" step="40"></div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="table-row">
-                    <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
-                      <div class="flex">
-                        <div class="w-1/2">General</div>
-                        <div><input type="number" v-model="general" class="min-w-full" min="0" max="50" step="50"></div>
-                      </div>
-                    </div>
-                  </div>
                   
                     <div class="table-row">
                         <div class="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">
